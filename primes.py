a181581c65e2ca02361abfaf8e5ac4cd97d779d6
Sur091 <91460166+Sur091@users.noreply.github.com>
2022-06-27 13:02:00 +0545

    I used numpy to speed things  up.
    Python is slow and wastes a lot of memory. By using NumPy, I sped things up.

--- primes.py
+++ primes.py
@@ -1,13 +1,15 @@
+import numpy as np
+
+
 def primes(num):
     l_ = (num - 1) // 2
-    pri = [True] * l_
+    pri = np.full(l_, True, bool)
 
     for a in range(int(num ** 0.5) // 2 + 1):
         if pri[a]:
-            start, step = 2 * a * a + 6 * a + 3, 2 * a + 3
-            pri[start::step] = [False] * len(range(start, l_, step))
+            pri[2 * a * a + 6 * a + 3::2 * a + 3] = False
 
-    return sum(pri) + 1
+    return np.sum(pri) + 1
 
 
-print(primes(100_000_000))
+print(primes(1_000_000_000))
